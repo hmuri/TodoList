e5477f467bed8388cffb1b161c1f7b5ac54ce6b3
macbook <rmk.ewha@ewhain.net>
2024-07-21 16:58:54 +0900

    delete todo list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(["Play", "Sleep", "Code"]);
   const [input, setInput] = useState("");
 
   const handleAddTask = () => {
@@ -15,6 +16,11 @@ function App() {
     setInput(event.target.value);
   };
 
+  const handleDelete = (index) => {
+    const newTasks = tasks.filter((task, i) => i !== index);
+    setTasks(newTasks);
+  };
+
   return (
     <div>
       <h1>ToDo List</h1>
@@ -22,11 +28,32 @@ function App() {
       <button onClick={handleAddTask}>추가</button>
       <div>
         {tasks.map((task, index) => (
-          <div key={index}>{task}</div>
+          <TaskBox>
+            {" "}
+            <div key={index}>
+              {task} {index}
+            </div>
+            <div>완료</div>
+            <DeleteText onClick={() => handleDelete(index)}>삭제</DeleteText>
+          </TaskBox>
         ))}
+        ###################################################
+        <div>완료된 일</div>
       </div>
     </div>
   );
 }
 
 export default App;
+
+const TaskBox = styled.div`
+  width: 500px;
+  background-color: lightblue;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const DeleteText = styled.div`
+  color: red;
+  cursor: pointer;
+`;
